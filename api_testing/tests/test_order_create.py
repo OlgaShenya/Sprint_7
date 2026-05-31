002459cdc515b5e1d1e4ca2f9df7cb512521b4b9
OlgaShenya <Olga.Shenya@gmail.com>
2026-05-31 12:19:42 +0300

    Fix: remove if-condition, use extra dict in parametrize

--- api_testing/tests/test_order_create.py
+++ api_testing/tests/test_order_create.py
@@ -8,18 +8,16 @@ from api_testing.tests.utils.assertions import assert_order_create_success
 @allure.title("Создание заказа: {test_name}")
 @allure.description("Проверка кода 201 и наличия track в ответе")
 @pytest.mark.parametrize(
-    "test_name,color",
+    "test_name,extra",
     [
-        ("BLACK only", ["BLACK"]),
-        ("GREY only", ["GREY"]),
-        ("Both colors", ["BLACK", "GREY"]),
-        ("No color", None),
+        ("BLACK only", {"color": ["BLACK"]}),
+        ("GREY only", {"color": ["GREY"]}),
+        ("Both colors", {"color": ["BLACK", "GREY"]}),
+        ("No color", {}),
     ],
 )
 class TestOrderCreate:
-    def test_create_order_with_color_returns_201(self, order_api_client, test_name, color):
-        payload = generate_order_payload()
-        if color is not None:
-            payload["color"] = color
+    def test_create_order_with_color_returns_201(self, order_api_client, test_name, extra):
+        payload = {**generate_order_payload(), **extra}
         response = order_api_client.create(payload)
         assert_order_create_success(response, test_name=test_name)
